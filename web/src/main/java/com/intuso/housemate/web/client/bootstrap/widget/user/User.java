f43bc0b2ed0b3504b081e73935bab2e1fd462f3b
tom <tom@d590dbca-1a8a-4b12-a93f-79df7afc4a52>
2013-10-12 16:24:35 +0000

    New UI and loads of other things

--- web/src/main/java/com/intuso/housemate/web/client/bootstrap/widget/user/User.java
+++ web/src/main/java/com/intuso/housemate/web/client/bootstrap/widget/user/User.java
@@ -1,11 +1,15 @@
 package com.intuso.housemate.web.client.bootstrap.widget.user;
 
+import com.github.gwtbootstrap.client.ui.Button;
+import com.github.gwtbootstrap.client.ui.Collapse;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
-import com.intuso.housemate.web.client.bootstrap.widget.command.PerformButton;
+import com.intuso.housemate.web.client.bootstrap.widget.object.Control;
 import com.intuso.housemate.web.client.object.GWTProxyUser;
 
 /**
@@ -18,12 +22,23 @@ public class User extends Composite {
     private static UserUiBinder ourUiBinder = GWT.create(UserUiBinder.class);
 
     @UiField
-    public PerformButton removeButton;
+    Button settings;
+    @UiField
+    Collapse settingsPanel;
+    @UiField(provided = true)
+    Control control;
 
     public User(final GWTProxyUser user) {
-
+        control = new Control(user);
         initWidget(ourUiBinder.createAndBindUi(this));
+    }
 
-        removeButton.setCommand(user.getRemoveCommand());
+    @UiHandler("settings")
+    public void settingsClicked(ClickEvent event) {
+        settings.setActive(!settings.isActive());
+        if(settings.isActive())
+            settingsPanel.show();
+        else
+            settingsPanel.hide();
     }
 }
