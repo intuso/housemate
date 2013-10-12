8caff6066ca368c736a27c80ac7f0cf188d36d5f
tom <tom@d590dbca-1a8a-4b12-a93f-79df7afc4a52>
2013-10-12 18:37:44 +0000

    WEB: Configurable object view more generic

--- web/src/main/java/com/intuso/housemate/web/client/bootstrap/widget/object/ConfigurableObject.java
+++ web/src/main/java/com/intuso/housemate/web/client/bootstrap/widget/object/ConfigurableObject.java
@@ -2,10 +2,13 @@ package com.intuso.housemate.web.client.bootstrap.widget.object;
 
 import com.github.gwtbootstrap.client.ui.Button;
 import com.github.gwtbootstrap.client.ui.Collapse;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -18,35 +21,49 @@ import com.google.gwt.user.client.ui.Widget;
  */
 public abstract class ConfigurableObject extends Composite {
 
-    private Widget settingsWidget = null;
-
-    @UiField
-    public Button settingsButton;
-    @UiField
-    public Collapse settingsCollapse;
-    @UiField
-    public SimplePanel settingsPanel;
-
     @Override
     protected void initWidget(Widget widget) {
-        super.initWidget(widget);
-        settingsCollapse.setDefaultOpen(false);
-        settingsButton.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                settingsButton.setActive(!settingsButton.isActive());
-
-                // create the settings if we don't already have it
-                if(settingsWidget == null) {
-                    settingsWidget = createSettingsWidget();
-                    settingsPanel.add(settingsWidget);
-                }
-
-                // toggle settings visibility
-                settingsCollapse.toggle();
-            }
-        });
+        FlowPanel flowPanel = new FlowPanel();
+        flowPanel.add(widget);
+        flowPanel.add(new Settings());
+        super.initWidget(flowPanel);
     }
 
     protected abstract Widget createSettingsWidget();
+
+    interface SettingsUiBinder extends UiBinder<Widget, Settings> {}
+
+    private static SettingsUiBinder ourUiBinder = GWT.create(SettingsUiBinder.class);
+
+    public class Settings extends Composite {
+
+        private Widget settingsWidget = null;
+
+        @UiField
+        public Button settingsButton;
+        @UiField
+        public Collapse settingsCollapse;
+        @UiField
+        public SimplePanel settingsPanel;
+
+        public Settings() {
+            initWidget(ourUiBinder.createAndBindUi(this));
+            settingsCollapse.setDefaultOpen(false);
+            settingsButton.addClickHandler(new ClickHandler() {
+                @Override
+                public void onClick(ClickEvent event) {
+                    settingsButton.setActive(!settingsButton.isActive());
+
+                    // create the settings if we don't already have it
+                    if(settingsWidget == null) {
+                        settingsWidget = createSettingsWidget();
+                        settingsPanel.add(settingsWidget);
+                    }
+
+                    // toggle settings visibility
+                    settingsCollapse.toggle();
+                }
+            });
+        }
+    }
 }
