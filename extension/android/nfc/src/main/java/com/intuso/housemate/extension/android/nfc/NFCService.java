8b4e62ea5768233995a3df9ec5af995d8f96f4e4
tom <tom@d590dbca-1a8a-4b12-a93f-79df7afc4a52>
2014-05-13 16:42:55 +0000

    Housemate: Android NFC. Scanned tag does command in a service rather than an activity

---
+++ extension/android/nfc/src/main/java/com/intuso/housemate/extension/android/nfc/NFCService.java
@@ -0,0 +1,91 @@
+package com.intuso.housemate.extension.android.nfc;
+
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.os.Binder;
+import android.os.IBinder;
+import android.widget.Toast;
+import com.intuso.housemate.api.comms.access.ApplicationDetails;
+import com.intuso.housemate.api.object.HousemateObject;
+import com.intuso.housemate.api.object.command.CommandPerformListener;
+import com.intuso.housemate.object.proxy.LoadManager;
+import com.intuso.housemate.object.proxy.simple.ProxyClientHelper;
+import com.intuso.housemate.platform.android.app.HousemateService;
+import com.intuso.housemate.platform.android.app.object.AndroidProxyCommand;
+import com.intuso.housemate.platform.android.app.object.AndroidProxyRoot;
+
+/**
+ * Created by tomc on 09/05/14.
+ */
+public class NFCService extends HousemateService {
+
+    public final static ApplicationDetails APPLICATION_DETAILS
+            = new ApplicationDetails(NfcActivity.class.getName(), "Android NFC", "Android NFC Service");
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new Binder();
+    }
+
+    @Override
+    public void onStart(Intent intent, final int startId) {
+        super.onStart(intent, startId);
+        if(NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            final String objectPath = intent.getData().getPath();
+            final ProxyClientHelper<AndroidProxyRoot> clientHelper = ProxyClientHelper.newClientHelper(getLog(),
+                    new AndroidProxyRoot(getLog(), getListenersFactory(), getProperties(), getRouter()), getRouter());
+            clientHelper.applicationDetails(APPLICATION_DETAILS)
+                    .load(objectPath.substring(1).split("/"))
+                    .callback(new LoadManager.Callback() {
+                        @Override
+                        public void failed(HousemateObject.TreeLoadInfo path) {
+                            message("Failed to load scanned tag's path", false);
+                            finished(clientHelper, startId);
+                        }
+
+                        @Override
+                        public void allLoaded() {
+                            final HousemateObject<?, ?, ?, ?> object = clientHelper.getRoot().getObject(objectPath.substring(1).split("/"));
+                            if(object == null) {
+                                message("Scanned tag's path does not exist", false);
+                                finished(clientHelper, startId);
+                            } else if (!(object instanceof AndroidProxyCommand)) {
+                                message("Scanned tag's path is not a command", false);
+                                finished(clientHelper, startId);
+                            } else {
+                                AndroidProxyCommand command = (AndroidProxyCommand) object;
+                                command.perform(new CommandPerformListener<AndroidProxyCommand>() {
+                                    @Override
+                                    public void commandStarted(AndroidProxyCommand command) {
+                                        // do nothing
+                                    }
+
+                                    @Override
+                                    public void commandFinished(AndroidProxyCommand command) {
+                                        message("Performed command " + command.getName(), true);
+                                        finished(clientHelper, startId);
+                                    }
+
+                                    @Override
+                                    public void commandFailed(AndroidProxyCommand command, String error) {
+                                        message("Performed command " + command.getName() + " failed: " + error, false);
+                                        finished(clientHelper, startId);
+                                    }
+                                });
+                            }
+                        }
+                    })
+                    .start();
+        }
+    }
+
+    protected void message(String message, boolean isShort) {
+        Toast.makeText(getApplicationContext(), message, isShort ? Toast.LENGTH_SHORT : Toast.LENGTH_LONG).show();
+    }
+
+    protected void finished(ProxyClientHelper<AndroidProxyRoot> clientHelper, int startId) {
+        if(clientHelper != null)
+            clientHelper.stop();
+        stopSelf(startId);
+    }
+}
--- extension/android/nfc/src/main/java/com/intuso/housemate/extension/android/nfc/NfcActivity.java
+++ extension/android/nfc/src/main/java/com/intuso/housemate/extension/android/nfc/NfcActivity.java
@@ -1,14 +1,8 @@
 package com.intuso.housemate.extension.android.nfc;
 
-import android.nfc.NfcAdapter;
-import android.widget.TextView;
-import com.intuso.housemate.api.comms.access.ApplicationDetails;
-import com.intuso.housemate.api.object.HousemateObject;
-import com.intuso.housemate.api.object.command.CommandPerformListener;
-import com.intuso.housemate.object.proxy.LoadManager;
+import android.content.Intent;
 import com.intuso.housemate.object.proxy.simple.ProxyClientHelper;
 import com.intuso.housemate.platform.android.app.HousemateActivity;
-import com.intuso.housemate.platform.android.app.object.AndroidProxyCommand;
 import com.intuso.housemate.platform.android.app.object.AndroidProxyRoot;
 
 /**
@@ -20,76 +14,15 @@ import com.intuso.housemate.platform.android.app.object.AndroidProxyRoot;
  */
 public class NfcActivity extends HousemateActivity {
 
-    public final static ApplicationDetails APPLICATION_DETAILS
-            = new ApplicationDetails(NfcActivity.class.getName(), "Android NFC", "Android NFC Service");
-
     private ProxyClientHelper<AndroidProxyRoot> clientHelper;
 
     @Override
     public void onStart() {
         super.onStart();
-        setContentView(R.layout.main);
-        setMessage("NfcActivity started");
-        if(NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-            final String objectPath = getIntent().getData().getPath();
-            setMessage("Tag scanned, path is "+ getIntent().getData().getPath());
-            clientHelper = ProxyClientHelper.newClientHelper(getLog(),
-                    new AndroidProxyRoot(getLog(), getListenersFactory(), getProperties(), getRouter()), getRouter());
-            clientHelper.applicationDetails(APPLICATION_DETAILS)
-                    .load(objectPath.substring(1).split("/"))
-                    .callback(new LoadManager.Callback() {
-                        @Override
-                        public void failed(HousemateObject.TreeLoadInfo path) {
-                            setMessage("Failed to load");
-                        }
-
-                        @Override
-                        public void allLoaded() {
-                            final HousemateObject<?, ?, ?, ?> object = clientHelper.getRoot().getObject(objectPath.substring(1).split("/"));
-                            if(object == null)
-                                setMessage(objectPath + " does not exist");
-                            if (!(object instanceof AndroidProxyCommand)) {
-                                setMessage(objectPath + " is not a command");
-                            } else {
-                                AndroidProxyCommand command = (AndroidProxyCommand) object;
-                                command.perform(new CommandPerformListener<AndroidProxyCommand>() {
-                                    @Override
-                                    public void commandStarted(AndroidProxyCommand command) {
-                                        setMessage("Performing");
-                                    }
-
-                                    @Override
-                                    public void commandFinished(AndroidProxyCommand command) {
-                                        setMessage("Performed");
-                                    }
-
-                                    @Override
-                                    public void commandFailed(AndroidProxyCommand command, String error) {
-                                        setMessage("Perform failed: " + error);
-                                    }
-                                });
-                            }
-                        }
-                    })
-                    .start();
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        if(clientHelper != null) {
-            clientHelper.stop();
-            clientHelper = null;
-        }
-        super.onStop();
-    }
-
-    private void setMessage(final String message) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                ((TextView) findViewById(R.id.onLabel)).setText(message);
-            }
-        });
+        Intent intent = new Intent(getApplicationContext(), NFCService.class);
+        intent.setAction(getIntent().getAction());
+        intent.setData(getIntent().getData());
+        getApplicationContext().startService(intent);
+        finish();
     }
 }
