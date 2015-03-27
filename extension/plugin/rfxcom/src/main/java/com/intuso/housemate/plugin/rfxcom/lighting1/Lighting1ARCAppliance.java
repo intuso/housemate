998dac4e4304b58b78e35bd61b022c430a259d4c
Tom Clabon <tom@intuso.com>
2015-03-27 21:59:49 +0000

    Lighting1ARCAppliance creates correct devices

--- extension/plugin/rfxcom/src/main/java/com/intuso/housemate/plugin/rfxcom/lighting1/Lighting1ARCAppliance.java
+++ extension/plugin/rfxcom/src/main/java/com/intuso/housemate/plugin/rfxcom/lighting1/Lighting1ARCAppliance.java
@@ -7,14 +7,13 @@ import com.intuso.housemate.plugin.api.TypeInfo;
 import com.intuso.housemate.plugin.rfxcom.RFXtrx433Hardware;
 import com.intuso.utilities.listener.ListenersFactory;
 import com.intuso.utilities.log.Log;
-import com.rfxcom.rfxtrx.util.lighting2.Lighting2Appliance;
 
 /**
  * Housemate device that controls a USB relay
  *
  */
 @TypeInfo(id = "rfxcom-lighting1-arc", name = "Lighting1 ARC", description = "RFXCom Lighting1 ARC Appliance")
-public class Lighting1ARCAppliance extends com.intuso.housemate.plugin.rfxcom.lighting2.Lighting2Appliance {
+public class Lighting1ARCAppliance extends Lighting1Appliance {
 
 	@Inject
 	public Lighting1ARCAppliance(Log log,
@@ -23,7 +22,7 @@ public class Lighting1ARCAppliance extends com.intuso.housemate.plugin.rfxcom.li
 		super(log, listenersFactory, "rfxcom-lighting1-arc", data);
 	}
 
-    public Lighting2Appliance createAppliance(int houseId, byte unitCode) {
-        return RFXtrx433Hardware.INSTANCE.makeLighting2AC(houseId, unitCode);
+    public com.rfxcom.rfxtrx.util.lighting1.Lighting1Appliance createAppliance(byte houseId, byte unitCode) {
+        return RFXtrx433Hardware.INSTANCE.makeLighting1ARC(houseId, unitCode);
     }
 }
