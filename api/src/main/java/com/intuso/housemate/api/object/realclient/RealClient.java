cfacacf5d4df5db53a5369fc36bbd211f3db7e1a
Tom Clabon <tom@intuso.com>
2015-03-30 08:50:12 +0100

    Added RealClient object to api

---
+++ api/src/main/java/com/intuso/housemate/api/object/realclient/RealClient.java
@@ -0,0 +1,41 @@
+package com.intuso.housemate.api.object.realclient;
+
+import com.intuso.housemate.api.object.BaseHousemateObject;
+import com.intuso.housemate.api.object.application.Application;
+import com.intuso.housemate.api.object.application.HasApplications;
+import com.intuso.housemate.api.object.automation.Automation;
+import com.intuso.housemate.api.object.automation.HasAutomations;
+import com.intuso.housemate.api.object.device.Device;
+import com.intuso.housemate.api.object.device.HasDevices;
+import com.intuso.housemate.api.object.hardware.Hardware;
+import com.intuso.housemate.api.object.hardware.HasHardwares;
+import com.intuso.housemate.api.object.list.List;
+import com.intuso.housemate.api.object.type.HasTypes;
+import com.intuso.housemate.api.object.type.Type;
+import com.intuso.housemate.api.object.user.HasUsers;
+import com.intuso.housemate.api.object.user.User;
+
+/**
+ */
+public interface RealClient<
+            APPLICATIONS extends List<? extends Application<?, ?, ?, ?, ?>>,
+            AUTOMATIONS extends List<? extends Automation<?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?>>,
+            DEVICES extends List<? extends Device<?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?>>,
+            HARDWARES extends List<? extends Hardware<?, ?>>,
+            TYPES extends List<? extends Type>,
+            USERS extends List<? extends User<?, ?>>,
+            COMMAND extends RealClient<?, ?, ?, ?, ?, ?, ?, ?>,
+            REALCLIENT extends RealClient<APPLICATIONS, AUTOMATIONS, DEVICES, HARDWARES, TYPES, USERS, COMMAND, REALCLIENT>>
+        extends BaseHousemateObject<RealClientListener<? super REALCLIENT>>,
+            HasTypes<TYPES>,
+            HasHardwares<HARDWARES>,
+            HasDevices<DEVICES>,
+            HasAutomations<AUTOMATIONS>,
+            HasApplications<APPLICATIONS>,
+            HasUsers<USERS> {
+
+    public COMMAND getAddAutomationCommand();
+    public COMMAND getAddDeviceCommand();
+    public COMMAND getAddHardwareCommand();
+    public COMMAND getAddUserCommand();
+}
