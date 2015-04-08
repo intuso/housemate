92e92d1ecc1d612208cf761e59fc912627c1b0c4
Tom Clabon <tom@intuso.com>
2015-04-08 16:31:09 +0100

    Android: Better network state receviers

--- extension/android/widget/src/main/java/com/intuso/housemate/extension/android/widget/NetworkStateReceiver.java
+++ extension/android/widget/src/main/java/com/intuso/housemate/extension/android/widget/NetworkStateReceiver.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Created by tomc on 02/06/14.
@@ -15,13 +17,39 @@ import android.net.NetworkInfo;
  */
 public class NetworkStateReceiver extends BroadcastReceiver {
 
+    private final static String TAG = "WidgetNetworkState";
+
     public void onReceive(Context context, Intent intent) {
+        debugIntent(intent);
         if(intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
-            NetworkInfo ni = intent.getParcelableExtra(ConnectivityManager.EXTRA_NETWORK_INFO);
+            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo ni = connectivityManager.getActiveNetworkInfo();
+            boolean connected = ni != null && ni.isConnectedOrConnecting();
+            if(!connected) {
+                Log.d(TAG, "Connected false, checking other network info");
+                ni = intent.getParcelableExtra(ConnectivityManager.EXTRA_OTHER_NETWORK_INFO);
+                connected = ni != null && ni.isFailover();
+            }
+            Log.d(TAG, "Parsed connected as " + connected);
             Intent serviceIntent = new Intent(context, WidgetService.class);
             serviceIntent.setAction(WidgetService.NETWORK_AVAILABLE_ACTION);
             serviceIntent.putExtra(WidgetService.NETWORK_AVAILABLE, ni != null && ni.isConnectedOrConnecting());
             context.startService(serviceIntent);
+        }
+    }
+
+    private void debugIntent(Intent intent) {
+        Log.v(TAG, "action: " + intent.getAction());
+        Log.v(TAG, "component: " + intent.getComponent());
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            for (String key: extras.keySet()) {
+                Log.v(TAG, "key [" + key + "]: " +
+                        extras.get(key));
+            }
+        }
+        else {
+            Log.v(TAG, "no extras");
         }
     }
 }
--- platform/android/service/src/main/java/com/intuso/housemate/platform/android/service/service/NetworkStateReceiver.java
+++ platform/android/service/src/main/java/com/intuso/housemate/platform/android/service/service/NetworkStateReceiver.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Created by tomc on 02/06/14.
@@ -15,13 +17,39 @@ import android.net.NetworkInfo;
  */
 public class NetworkStateReceiver extends BroadcastReceiver {
 
+    private final static String TAG = "PlatformNetworkState";
+
     public void onReceive(Context context, Intent intent) {
+        debugIntent(intent);
         if(intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
-            NetworkInfo ni = intent.getParcelableExtra(ConnectivityManager.EXTRA_NETWORK_INFO);
+            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo ni = connectivityManager.getActiveNetworkInfo();
+            boolean connected = ni != null && ni.isConnectedOrConnecting();
+            if(!connected) {
+                Log.d(TAG, "Connected false, checking other network info");
+                ni = intent.getParcelableExtra(ConnectivityManager.EXTRA_OTHER_NETWORK_INFO);
+                connected = ni != null && ni.isFailover();
+            }
+            Log.d(TAG, "Parsed connected as " + connected);
             Intent serviceIntent = new Intent(context, ConnectionService.class);
             serviceIntent.setAction(ConnectionService.NETWORK_AVAILABLE_ACTION);
-            serviceIntent.putExtra(ConnectionService.NETWORK_AVAILABLE, ni != null && ni.isConnectedOrConnecting());
+            serviceIntent.putExtra(ConnectionService.NETWORK_AVAILABLE, connected);
             context.startService(serviceIntent);
+        }
+    }
+
+    private void debugIntent(Intent intent) {
+        Log.d(TAG, "action: " + intent.getAction());
+        Log.d(TAG, "component: " + intent.getComponent());
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            for (String key: extras.keySet()) {
+                Log.d(TAG, "key [" + key + "]: " +
+                        extras.get(key));
+            }
+        }
+        else {
+            Log.d(TAG, "no extras");
         }
     }
 }
