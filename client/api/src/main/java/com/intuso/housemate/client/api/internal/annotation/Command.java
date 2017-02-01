ed55c81638ec5a10400ff8ba6de7ea3a334ceefe
Tom Clabon <tom@intuso.com>
2017-02-01 11:36:20 +0000

    Update Command annotation javadoc

--- client/api/src/main/java/com/intuso/housemate/client/api/internal/annotation/Command.java
+++ client/api/src/main/java/com/intuso/housemate/client/api/internal/annotation/Command.java
@@ -6,8 +6,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotate a method with this to create a command for your object. All arguments for the command
- * must have {@link Parameter} annotations
+ * Annotate a method with this to create a command for your object. All arguments for the method must have {@link Id}
+ * annotations and optionally a {@link Parameter} annotation to use restrictions or control min and max num values
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
